903db7e11fc41c7832c192f437ef39aa4c5536d6
kiviacelia <kiviacelia@gmail.com>
2019-09-15 05:04:42 -0300

    initial comit

---
+++ src/DAO/AuthorsDAO.java
@@ -0,0 +1,12 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class AuthorsDAO {
+    
+  
+
+    
+}
---
+++ src/Model/BooksAuthorsModel.java
@@ -0,0 +1,35 @@
+package Model;
+public class BooksAuthorsModel {
+    
+    private String isbn;
+    private int authorId;
+    private int seqNo;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public int getSeqNo() {
+        return seqNo;
+    }
+
+    public void setSeqNo(int seqNo) {
+        this.seqNo = seqNo;
+    }
+    
+   
+    
+    
+}
